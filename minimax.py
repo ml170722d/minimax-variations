1480035c1b1c6e0d92181172cb8f2f968ab980a6
Luka <ml170722.etf@gmail.com>
2021-12-18 20:37:38 +0100

    Expectimax alg implemented

---
+++ minimax.py
@@ -0,0 +1,182 @@
+import math
+from states import GameState
+
+
+class Node:
+    def __init__(self, state: GameState, direction: str = ''):
+        self.state = state
+        self.dir = direction
+        pass
+
+    def successors(self, agent_id: int) -> list:
+        pass
+
+    def get_state(self) -> GameState:
+        return self.state
+
+    # def get_type(self) -> Type:
+    #     return self.type
+
+    def get_direction(self) -> str:
+        return self.dir
+
+    def is_terminal(self, agent_id: int) -> bool:
+        return True if len(self.state.get_legal_actions(agent_id)) == 0 else False
+
+    def get_rival_ids(self, agent_id: int) -> list[int]:
+        # all_agents = [agent.id for agent in self.state.agents]
+        # all_agents.remove(agent_id)
+        # return all_agents
+        return [agent.id for agent in self.state.agents if agent_id != agent.get_id()]
+
+
+class Minimax:
+    class MaxNode(Node):
+        def successors(self, agent_id: int):
+            actions = self.state.get_legal_actions(agent_id)
+            states_list = [self.state.apply_action(agent_id, act) for act in actions]
+            successors = [Minimax.MinNode(states_list[i], actions[i])
+                          for i in range(len(states_list))]
+            return successors
+
+    class MinNode(Node):
+        def successors(self, agent_id: int):
+            actions = self.state.get_legal_actions(agent_id)
+            states_list = [self.state.apply_action(agent_id, act) for act in actions]
+            successors = [Minimax.MaxNode(states_list[i], actions[i])
+                          for i in range(len(states_list))]
+            return successors
+
+    @staticmethod
+    def eval(node: Node, agent_id: int) -> float:
+        curr_agent_eval = len(node.get_state().get_legal_actions(agent_id))
+        rival_ids = node.get_rival_ids(agent_id)
+        rival_agent_eval = sum(len(node.get_state().get_legal_actions(rival_id)) for rival_id in rival_ids)
+        rival_agent_eval = rival_agent_eval / len(rival_ids)
+        return 10 * (curr_agent_eval - rival_agent_eval)
+
+    @staticmethod
+    def is_terminal(node: Node, curr_agent_id: int) -> bool:
+        return node.is_terminal(curr_agent_id)
+
+    def run(self, node: Node, depth: int, curr_agent_id: int) -> (float, Node):
+        if self.is_terminal(node, curr_agent_id) or depth == 0:
+            return self.eval(node, curr_agent_id), node
+
+        if isinstance(node, Minimax.MaxNode):
+            # MAX
+            score = -math.inf
+            n = None
+            for s in node.successors(curr_agent_id):
+                tmp, n_tmp = self.run(s, depth - 1, curr_agent_id)
+                if score < tmp:
+                    score = tmp
+                    n = s
+
+            return score, n
+        else:
+            # MIN
+            score = math.inf
+            n = None
+            rival_id = node.get_rival_ids(curr_agent_id)
+            for s in node.successors(rival_id[0]):
+                tmp, n_tmp = self.run(s, depth - 1, curr_agent_id)
+                if score > tmp:
+                    score = tmp
+                    n = s
+
+            return score, n
+
+
+class MinimaxAB(Minimax):
+
+    def run(self, node: Node, depth: int, curr_agent_id: int, alpha: float, beta: float) -> (float, Node):
+        if self.is_terminal(node, curr_agent_id) or depth == 0:
+            return self.eval(node, curr_agent_id), node
+
+        if isinstance(node, Minimax.MaxNode):
+            # MAX
+            score = -math.inf
+            n = None
+            for s in node.successors(curr_agent_id):
+                tmp, n_tmp = self.run(s, depth - 1, curr_agent_id, alpha, beta)
+                if score < tmp:
+                    score = tmp
+                    n = s
+                alpha = max(alpha, score)
+                if alpha >= beta:
+                    print('ab cut')
+                    break
+
+            return score, n
+        else:
+            # MIN
+            score = math.inf
+            n = None
+            rival_id = node.get_rival_ids(curr_agent_id)
+            for s in node.successors(rival_id[0]):
+                tmp, n_tmp = self.run(s, depth - 1, curr_agent_id, alpha, beta)
+                if score > tmp:
+                    score = tmp
+                    n = s
+                alpha = max(alpha, score)
+                if alpha >= beta:
+                    print('ab cut')
+                    break
+
+            return score, n
+
+
+class Expectimax(Minimax):
+    class MaxNode(Node):
+        def successors(self, agent_id: int) -> list:
+            actions = self.state.get_legal_actions(agent_id)
+            states_list = [self.state.apply_action(agent_id, act) for act in actions]
+            successors = [Expectimax.ChanceNode(states_list[i], actions[i])
+                          for i in range(len(states_list))]
+            return successors
+
+    class ChanceNode(Node):
+        def successors(self, agent_id: int) -> list:
+            actions = self.state.get_legal_actions(agent_id)
+            states_list = [self.state.apply_action(agent_id, act) for act in actions]
+            successors = [Expectimax.MaxNode(states_list[i], actions[i])
+                          for i in range(len(states_list))]
+            return successors
+
+    def run(self, node: Node, depth: int, curr_agent_id: int) -> (float, Node):
+        if self.is_terminal(node, curr_agent_id) or depth == 0:
+            return self.eval(node, curr_agent_id), node
+
+        if isinstance(node, Expectimax.MaxNode):
+            # MAX
+            score = -math.inf
+            n = None
+            for s in node.successors(curr_agent_id):
+                tmp, n_tmp = self.run(s, depth - 1, curr_agent_id)
+                if score < tmp:
+                    score = tmp
+                    n = s
+
+            return score, n
+        else:
+            # CHANCE
+            score = 0
+            n = None
+            rival_id = node.get_rival_ids(curr_agent_id)
+            successors = node.successors(rival_id[0])
+            for s in successors:
+                prob = 1 / len(successors)
+                tmp, n_tpm = self.run(s, depth - 1, curr_agent_id)
+                score += prob * tmp
+                n = node
+
+            return score, n
+
+
+def print_map(state: GameState):
+    print('---------------')
+    for row in state.char_map:
+        print(row)
+    print('---------------')
+    pass
--- students.py
+++ students.py
@@ -1,9 +1,8 @@
 import math
 import random
-import enum
 
 from agents import Agent
-from states import GameState
+from minimax import Minimax, MinimaxAB, Expectimax
 
 
 # Example agent, behaves randomly.
@@ -33,27 +32,31 @@ class StudentAgent(Agent):
 class MinimaxAgent(StudentAgent):
 
     def get_next_action(self, state, max_levels):
-        node = Node(state, Node.Type.MAX, '')
+        node = Minimax.MaxNode(state)
         alg = Minimax()
 
-        score, node = alg.minimax(node, max_levels, self.id)
+        score, node = alg.run(node, max_levels, self.get_id())
         return node.get_direction()
 
 
 class MinimaxABAgent(StudentAgent):
 
     def get_next_action(self, state, max_levels):
-        node = Node(state, Node.Type.MAX, '')
+        node = MinimaxAB.MaxNode(state)
         alg = MinimaxAB()
 
-        score, node = alg.minimax(node, max_levels, self.id, -math.inf, math.inf)
+        score, node = alg.run(node, max_levels, self.get_id(), -math.inf, math.inf)
         return node.get_direction()
 
 
 class ExpectAgent(StudentAgent):
 
     def get_next_action(self, state, max_levels):
-        pass
+        node = Expectimax.MaxNode(state)
+        alg = Expectimax()
+
+        score, node = alg.run(node, max_levels, self.get_id())
+        return node.get_direction()
 
 
 class MaxNAgent(StudentAgent):
@@ -61,162 +64,3 @@ class MaxNAgent(StudentAgent):
     def get_next_action(self, state, max_levels):
         pass
 
-
-class Node:
-    class Type(enum.Enum):
-        MAX = 'max'
-        MIN = 'min'
-
-    def __init__(self, state: GameState, node_type: Type, direction: str):
-        self.type = node_type
-        self.state = state
-        self.dir = direction
-        pass
-
-    def successors(self, agent_id: int):
-        actions = self.state.get_legal_actions(agent_id)
-        states_list = [self.state.apply_action(agent_id, act) for act in actions]
-
-        _type = Node.Type.MAX if self.type != Node.Type.MAX else Node.Type.MIN
-        successors = [Node(states_list[i], _type, actions[i])
-                      for i in range(len(states_list))]
-
-        return successors
-
-    def get_state(self) -> GameState:
-        return self.state
-
-    def get_type(self) -> Type:
-        return self.type
-
-    def get_direction(self) -> str:
-        return self.dir
-
-    def is_terminal(self, agent_id: int) -> bool:
-        actions = self.state.get_legal_actions(agent_id)
-
-        if len(actions) == 0:
-            return True
-        return False
-
-    def get_rival_ids(self, agent_id: int) -> list[int]:
-        all_agents = [agent.id for agent in self.state.agents]
-        all_agents.remove(agent_id)
-        return all_agents
-
-
-class Minimax:
-    #           |   id  |   role
-    # ---------------------------
-    # STUDENT   |   0   |   MAX
-    # PROFESSOR |   1   |   MIN
-
-    def eval(self, node: Node) -> int:
-        student = len(node.state.get_legal_actions(0))
-        professor = len(node.state.get_legal_actions(1))
-        r = 0
-
-        # if node.get_type() == Node.Type.MAX:
-        #     r = (professor - student) * 10
-        # else:
-        #     r = (student - professor) * 10
-        r = (student - professor) * 10
-
-        # print_map(node.get_state())
-        # print(node.get_type(), r)
-        return r
-
-    def is_terminal(self, node: Node, curr_agent_id: int) -> bool:
-        return node.is_terminal(curr_agent_id)
-
-    def minimax(self, node: Node, depth: int, curr_agent_id: int) -> (int, Node):
-        # print_map(node.get_state())
-        # print('type:', 'MAX' if node.get_type() != Node.Type.MAX else 'MIN', f'on depth {depth}')
-
-        if self.is_terminal(node, curr_agent_id) or depth == 0:
-            return self.eval(node), node
-
-        if node.get_type() == Node.Type.MAX:
-            # MAX
-            score = -math.inf
-            n = None
-            for s in node.successors(curr_agent_id):
-                tmp, n_tmp = self.minimax(s, depth - 1, curr_agent_id)
-                # score = max(score, tmp)
-                if score < tmp:
-                    score = tmp
-                    n = s
-
-            return score, n
-        else:
-            # MIN
-            score = math.inf
-            n = None
-            for rival_id in node.get_rival_ids(curr_agent_id):
-                for s in node.successors(rival_id):
-                    tmp, n_tmp = self.minimax(s, depth - 1, curr_agent_id)
-                    # score = min(score, tmp)
-                    if score > tmp:
-                        score = tmp
-                        n = s
-
-            return score, n
-
-
-class MinimaxAB(Minimax):
-
-    def eval(self, node: Node, agent_id: int) -> float:
-        curr_agent_eval = len(node.get_state().get_legal_actions(agent_id))
-        rival_ids = node.get_rival_ids(agent_id)
-        rival_agent_eval = sum(len(node.get_state().get_legal_actions(rival_id)) for rival_id in rival_ids)
-        rival_agent_eval = rival_agent_eval / len(rival_ids)
-        return 10 * (curr_agent_eval - rival_agent_eval)
-
-    def minimax(self, node: Node, depth: int, curr_agent_id: int, alpha: float, beta: float) -> (int, Node):
-        # print_map(node.get_state())
-        # print('type:', 'MAX' if node.get_type() != Node.Type.MAX else 'MIN', f'on depth {depth}')
-
-        if self.is_terminal(node, curr_agent_id) or depth == 0:
-            return self.eval(node, curr_agent_id), node
-
-        if node.get_type() == Node.Type.MAX:
-            # MAX
-            score = -math.inf
-            n = None
-            for s in node.successors(curr_agent_id):
-                tmp, n_tmp = self.minimax(s, depth - 1, curr_agent_id, alpha, beta)
-                # score = max(score, tmp)
-                if score < tmp:
-                    score = tmp
-                    n = s
-                alpha = max(alpha, score)
-                if alpha >= beta:
-                    print('ab cut')
-                    break
-
-            return score, n
-        else:
-            # MIN
-            score = math.inf
-            n = None
-            for rival_id in node.get_rival_ids(curr_agent_id):
-                for s in node.successors(rival_id):
-                    tmp, n_tmp = self.minimax(s, depth - 1, curr_agent_id, alpha, beta)
-                    # score = min(score, tmp)
-                    if score > tmp:
-                        score = tmp
-                        n = s
-                    alpha = max(alpha, score)
-                    if alpha >= beta:
-                        print('ab cut')
-                        break
-
-            return score, n
-
-
-def print_map(state: GameState):
-    print('---------------')
-    for row in state.char_map:
-        print(row)
-    print('---------------')
-    pass
